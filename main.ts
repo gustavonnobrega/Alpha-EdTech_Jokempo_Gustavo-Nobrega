67dec3fb06e45a3e895e6f5ac7a527550fd99ae0
Gustavo Nóbrega <37496116+gustavonnobrega@users.noreply.github.com>
2021-01-16 10:14:55 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,144 @@
+input.onButtonPressed(Button.A, function () {
+    if (user_choice == 0) {
+        user_choice = 2
+    } else {
+        user_choice += -1
+    }
+    if (user_choice == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (user_choice == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (user_choice == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # . # #
+            . . # . .
+            # # . # #
+            # # . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # . . . .
+            # # . # #
+            # . . # .
+            # # . # .
+            `)
+        user_choice = 0
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    machine_choice = randint(0, 2)
+    if (machine_choice == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (machine_choice == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (machine_choice == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # . # #
+            . . # . .
+            # # . # #
+            # # . . .
+            `)
+    }
+    if (user_choice == 0 && machine_choice == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        user_choice += 1
+    } else if (user_choice == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        user_choice += 1
+    } else {
+        basic.showLeds(`
+            # # . . .
+            # # . # #
+            . . # . .
+            # # . # #
+            # # . . .
+            `)
+        user_choice += -2
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (user_choice == 2) {
+        user_choice = 0
+    } else {
+        user_choice += 1
+    }
+    if (user_choice == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (user_choice == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (user_choice == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # . # #
+            . . # . .
+            # # . # #
+            # # . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . # #
+            # . . . .
+            # # . # #
+            # . . # .
+            # # . # .
+            `)
+        user_choice = 0
+    }
+})
+let machine_choice = 0
+let user_choice = 0
+user_choice = 0
+basic.forever(function () {
+	
+})
